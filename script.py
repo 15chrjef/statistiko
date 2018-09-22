4c9841ee6c43237851a11287927bcccb4fbf002e
livinginformation <>
2018-09-22 12:52:59 -0700

    add top-ten to output

--- script.py
+++ script.py
@@ -6,6 +6,9 @@ import operator
 
 bad_chars = '()'
 data = []
+candidate_list = []
+current_price = float(input('What is the last price in the data set?: '))
+
 with open("output") as infile:
     for line in infile:
         for c in bad_chars: 
@@ -14,10 +17,22 @@ with open("output") as infile:
         obj = tuple(map(float, line.split(',')))
         data.append(obj)
 
+        (x,y,q,b) = tuple(map(float, line.split(',')))
+        candidate = (x,y,q*current_price+b)
+        candidate_list.append(candidate)
+
+
+candidate_list.sort(key=lambda x: x[2])
+top_ten = list(reversed(candidate_list[-10:]))
+
+print("Top ten:")
+for element in top_ten:
+	print(element)
+
 x, y, q, b = zip(*data)
-current_price = float(input('What is the current price?: '))
+
 mult = tuple([current_price*x for x in q])
-print(mult)
+#print(mult)
 z = tuple(map(sum, zip(mult, b)))
 z = list(map(float, z))
 grid_x, grid_y = np.mgrid[min(x):max(x):100j, min(y):max(y):100j]
